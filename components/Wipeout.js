279aefbdb9429259d9e262598dbef9ef250ee54e
Brandi Cameron <hello@brandicameron.com>
2022-11-07 20:43:01 -0600

    Trying without next/image component, may be causing bug on first render

--- components/Wipeout.js
+++ components/Wipeout.js
@@ -1,30 +1,30 @@
 import styles from './Wipeout.module.css';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 export default function Wipeout({ wipeout }) {
   // this bit of code fixes bug where png images wouldn't show
   // https://stackoverflow.com/questions/71392729/next-image-component-url-issue
-  const loaderProp = ({ src }) => {
-    return src;
-  };
+  // const loaderProp = ({ src }) => {
+  //   return src;
+  // };
 
   return (
     wipeout && (
       <main className={styles.mainWipeout}>
-        <Image
+        <img
           src='/../images/neuralyzer.png'
           alt='Neuralyzer - Prepare for Wipeout.'
           width={125}
           height={735}
-          loader={loaderProp}
+          // loader={loaderProp}
         />
-        <Image
+        <img
           className={styles.light}
           src='/../images/light-burst.png'
           alt=''
           width={300}
           height={200}
-          loader={loaderProp}
+          // loader={loaderProp}
         />
       </main>
     )
