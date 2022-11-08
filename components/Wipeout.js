ab747ca71038938c083cccd66357d5ea88d56e5b
Brandi Cameron <hello@brandicameron.com>
2022-11-07 21:14:11 -0600

    Added neuralyzer and light to separate div and added the lift animation to that div
    This prevents the bug where the neuralyzer didn't animate correctly the first time the site was loaded on ios

--- components/Wipeout.js
+++ components/Wipeout.js
@@ -1,31 +1,34 @@
 import styles from './Wipeout.module.css';
-// import Image from 'next/image';
+import Image from 'next/image';
 
 export default function Wipeout({ wipeout }) {
   // this bit of code fixes bug where png images wouldn't show
   // https://stackoverflow.com/questions/71392729/next-image-component-url-issue
-  // const loaderProp = ({ src }) => {
-  //   return src;
-  // };
+  const loaderProp = ({ src }) => {
+    return src;
+  };
 
   return (
     wipeout && (
       <main className={styles.mainWipeout}>
-        <img
-          src='/../images/neuralyzer.png'
-          alt='Neuralyzer - Prepare for Wipeout.'
-          width={125}
-          height={735}
-          // loader={loaderProp}
-        />
-        <img
-          className={styles.light}
-          src='/../images/light-burst.png'
-          alt=''
-          width={300}
-          height={200}
-          // loader={loaderProp}
-        />
+        <div className={styles.neuralyzerContainer}>
+          <Image
+            className={styles.neuralyzer}
+            src='/../images/neuralyzer.png'
+            alt='Neuralyzer - Prepare for Wipeout.'
+            width={125}
+            height={735}
+            loader={loaderProp}
+          />
+          <Image
+            className={styles.light}
+            src='/../images/light-burst.png'
+            alt=''
+            width={300}
+            height={200}
+            loader={loaderProp}
+          />
+        </div>
       </main>
     )
   );
