9102d1f18e4b647d885855fcb43c52f35d1ff3ab
Brandi Cameron <hello@brandicameron.com>
2022-11-30 12:29:42 -0600

    Moved wipeout redirect to wipeout component

--- components/Login/Wipeout.js
+++ components/Login/Wipeout.js
@@ -1,9 +1,25 @@
 import styles from './Wipeout.module.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Neuralyzer from '../../public/images/neuralyzer.png';
 import Light from '../../public/images/light-burst.png';
 
 export default function Wipeout({ wipeout }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (wipeout) {
+      const timer = setTimeout(() => {
+        router.push(`https://www.google.com/`);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [wipeout]);
+
   return (
     wipeout && (
       <main className={styles.mainWipeout}>
