511f29b144e0089020d0017fa11e14d245716ccc
Brandi Cameron <hello@brandicameron.com>
2022-12-01 14:30:38 -0600

    Adjusted frame media queries

--- pages/portal.js
+++ pages/portal.js
@@ -3,7 +3,8 @@ import Layout from '../components/Layout/Layout';
 import TargetOverview from '../components/TargetOverview/TargetOverview';
 
 export default function Portal() {
-  return <TargetOverview />;
+  // return <TargetOverview />;
+  return <h1></h1>;
 }
 
 Portal.getLayout = function getLayout(page) {
