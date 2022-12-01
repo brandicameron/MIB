5892c0bf66a97ebd5b1d7c73c00d8529b5803a1f
Brandi Cameron <hello@brandicameron.com>
2022-12-01 17:00:22 -0600

    Added custom time greeting to header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,9 +1,26 @@
 import styles from './Header.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+  const [greeting, setGreeting] = useState('Hello');
+
+  useEffect(() => {
+    const date = new Date();
+    const hour = date.getHours();
+
+    if (hour > 0 && hour < 12) {
+      setGreeting('Good morning');
+    }
+    if (hour >= 12 && hour < 17) {
+      setGreeting('Good afternoon');
+    } else {
+      setGreeting('Good evening');
+    }
+  }, []);
+
   return (
     <header className={styles.header}>
-      <p>Good morning Agent J</p>
+      <p>{greeting}, Agent J</p>
       <small className={styles.status}>
         <span className='bold'>STATUS:</span> <span className='success'>Active</span>
       </small>
